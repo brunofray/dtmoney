60601308a197be3427badde4c0ba27a58fc1e541
Bruno Fray <brunofray@gmail.com>
2021-03-30 19:24:33 -0300

    Configurando modal de criação

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -23,13 +23,13 @@ export function TransactionsTable() {
         <tbody>
           <tr>
             <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
+            <td className="deposit">R$12.000,00</td>
             <td>Desenvolvimento</td>
             <td>20/02/2021</td>
           </tr>
           <tr>
             <td>Aluguel</td>
-            <td className="withdraw">- R$1.100</td>
+            <td className="withdraw">- R$1.100,00</td>
             <td>Casa</td>
             <td>17/02/2021</td>
           </tr>
